93c9fd705ccb21462b081ca6742bb355e87bf5d4
Joshua <krealsion@gmail.com>
2018-11-24 13:37:25 -0700

    Work for test map creation

--- Gui.cs
+++ Gui.cs
@@ -13,8 +13,7 @@ namespace DwarfFortress
 
         public Gui()
         {
-            CameraOffset = new Point(0,0);
-
+            CameraOffset = new Point();
         }
 
         public void Draw(Map map)
