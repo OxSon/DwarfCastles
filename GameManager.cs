0f80a6632f981738d3354d81aedde7b533d228d6
Joshua <krealsion@gmail.com>
2018-11-24 15:39:42 -0700

    Created Basic Test for Pathing and Gui

--- GameManager.cs
+++ GameManager.cs
@@ -1,4 +1,7 @@
 using System;
+using System.CodeDom;
+using System.Drawing;
+using System.Threading;
 
 namespace DwarfFortress
 {
@@ -13,21 +16,51 @@ namespace DwarfFortress
         {
             Map = map;
             Gui = gui;
+            Run();
         }
 
         public Map Map { get; }
         public Gui Gui { get; }
 
+        private bool Running = true;
+        
+        public void Run()
+        {
+            while (Running)
+            {
+                Update();
+                Thread.Sleep(1000);
+            }
+        }
+
         public void Update()
         {
-            //TODO
-            throw new NotImplementedException();
+            foreach (Entity e in Map.Entities)
+            {
+                if (e is Actor)
+                {
+                    Actor a = (Actor) e;
+                    if (a.Tasks.Count == 0)
+                    {
+                        Random r = new Random();
+                        
+                        Point nextPos = new Point();
+                        do
+                        {
+                            nextPos = new Point(r.Next(0, Map.Size.X), r.Next(0, Map.Size.Y));
+                        } while (Map.Impassables[nextPos.X,nextPos.Y]);
+                        
+                        Task t = new Task(0, nextPos);
+                        a.Tasks.Add(t);
+                    }
+                    a.Update();
+                }
+            }
+            Gui.Draw(Map);
         }
 
         public void HandleInput()
         {
-            //TODO
-            throw new NotImplementedException();
         }
     }
 }
