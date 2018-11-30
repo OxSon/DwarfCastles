b81e7c447c2e612013be6088c10ea67bdf4d0183
Joshua <krealsion@gmail.com>
2018-11-30 12:25:33 -0700

    Completed working Resource Parser

--- ResourceMasterList.cs
+++ ResourceMasterList.cs
@@ -1,7 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using DwarfFortress;
+
 namespace DwarfCastles
 {
-    public class ResourceMasterList
+    public static class ResourceMasterList
     {
-        
+        private static IDictionary<string, Entity> DefaultEntities;
+
+        static ResourceMasterList()
+        {
+            DefaultEntities = new Dictionary<string, Entity>();
+            LoadAllResources();
+        }
+
+        private static void LoadAllResources()
+        {
+            
+        }
+
+        public static Entity GetDefaultClone(string name)
+        {
+            return GetDefault(name).Clone();
+        }
+
+        public static Entity GetDefault(string name)
+        {
+            return DefaultEntities[name];
+        }
     }
 }
