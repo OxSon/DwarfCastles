2b9b852ebf4744072887019cacc78282659df22a
Joshua <krealsion@gmail.com>
2018-12-01 10:48:18 -0700

    Started Implementation for Build

--- Jobs/Build.cs
+++ Jobs/Build.cs
@@ -1,10 +1,23 @@
+using System.Drawing;
+
 namespace DwarfCastles.Jobs
 {
     public class Build : Job
     {
+        private double WorkRequired;
+        
+        public Build(Point Position, string name)
+        {
+            var workRequired = ResourceMasterList.GetDefault(name).GetTag("buildable.workrequired");
+            if (workRequired != null)
+            {
+                WorkRequired = workRequired.Value.GetDouble();
+            }
+        }
+        
         public override void Work()
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
